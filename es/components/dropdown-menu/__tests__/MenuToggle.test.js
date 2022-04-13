694b4b262cbe69b9316bef0f3f2ab2a930e6309f
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2022-04-13 13:55:16 +0400

    refactor(project): add es directory build from installing buie as a regular dep

--- es/components/dropdown-menu/__tests__/MenuToggle.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MenuToggle from '../MenuToggle';
-describe('components/dropdown-menu/MenuToggle', function () {
-  test('should correctly render toggle', function () {
-    var wrapper = shallow(React.createElement(MenuToggle, null, "hi"));
-    expect(wrapper).toMatchSnapshot();
-  });
-});
--- es/icons/general/__tests__/IconPageBack.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import IconPageBack from '../IconPageBack';
-describe('icons/general/IconPageBack', function () {
-  test('should correctly render default paging back icon', function () {
-    var wrapper = shallow(React.createElement(IconPageBack, null));
-    expect(wrapper.hasClass('icon-page-back')).toBe(true);
-  });
-});
